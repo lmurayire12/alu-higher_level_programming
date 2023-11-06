68bddb2884f0c733b668230ab260445fb0857d85
lmurayire12 <l.murayire@alustudent.com>
2023-11-06 07:50:50 -0800

    new list with new element

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,11 +2,11 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-        elif idx > len(my_list) - 1:
-            return my_list
-        else:
-            new_list = []
-            for i in my_list:
-                new_list.append(i)
-            new_list[idx] = element
-            return new_list
+    elif idx > len(my_list) - 1:
+        return my_list
+    else:
+        new_list = []
+        for i in my_list:
+            new_list.append(i)
+        new_list[idx] = element
+        return new_list
