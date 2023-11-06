e261780c840ef3f6c8c1968d88ee706ccf1bcd64
lmurayire12 <l.murayire@alustudent.com>
2023-11-06 05:14:13 -0800

    print an element from a certain index

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx<0:
         return "none"
-    elif idx>len(my_list):
+    elif idx>len(my_list) -1:
         return "none"
     else:
         return my_list[idx]
