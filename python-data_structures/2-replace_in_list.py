668190058e5e0ee707df75761682a1353261379a
lmurayire12 <l.murayire@alustudent.com>
2023-11-06 06:22:27 -0800

    replacing an element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -5,8 +5,6 @@ def replace_in_list(my_list, idx, element):
     elif idx > len(my_list) - 1:
         return my_list
     else:
-        new_list = []
-        for i in my_list:
-            new_list.append(i)
+        new_list = my_list
         new_list[idx] = element
         return new_list
