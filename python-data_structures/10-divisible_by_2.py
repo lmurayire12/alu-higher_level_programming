2280f3566dc333d7fb40df04247848d2d64ded1b
lmurayire12 <l.murayire@alustudent.com>
2023-11-08 03:22:14 -0800

    dividends of 2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    for a in ls:
+        if ls[i]%2 == 0:
+            new_list.append(True)
+            i = i+1
+        else:
+            new_list.append(False)
+            i = i+1
