9ad83c4cd63ae135ff7d57639a2a660e2a4391f2
lmurayire12 <l.murayire@alustudent.com>
2023-11-10 07:07:53 -0800

    safe print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             num += 1
         except IndexError:
             break
-        print("")
-        return
+    print("")
+    return
                                                                                 
