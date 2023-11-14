db717f80b98ce8fbe8f04fbe918db4937c277fb7
lmurayire12 <l.murayire@alustudent.com>
2023-11-14 05:14:48 -0800

    current square

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""This is a class that defines the square"""
+
+
+class Square:
+        """Another square"""
+
+        def __init__(self, size=0):
+            """A new sqaure now"""
+
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+
+            self.__size = size
+        def area(self):
+            """Returns the current square area"""
+            return (size * size)
