ca2a7c475a8737708a6e631c422af816620a2fc5
lmurayire12 <l.murayire@alustudent.com>
2023-11-14 04:45:29 -0800

    2 square

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defines a square"""
+
+class Sqaure:
+    """Represents a square"""
+    
+    def __init__(self, size = 0):
+        """Initialize a new square"""
+    
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif self.size < 0:
+        raise ValueError("size must be >=0")
+    self.__size = size
+
+        
