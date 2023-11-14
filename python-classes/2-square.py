b3140e2ecf581a8173aa68d62b4fb26087733cfc
lmurayire12 <l.murayire@alustudent.com>
2023-11-14 04:53:56 -0800

    2 square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,7 +10,7 @@ class Sqaure:
     
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    elif self.size < 0:
+    elif size < 0:
         raise ValueError("size must be >=0")
     self.__size = size
 
