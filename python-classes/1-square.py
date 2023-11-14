b766e4f1965e51c88e15fa3f9dbc16ed80b6f7aa
lmurayire12 <l.murayire@alustudent.com>
2023-11-14 03:31:25 -0800

    square wize no size & __size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
 """This is a class that defines the square"""
 
 
 class Square:
     """Another square"""
-    def __init__(self,size)
-    """A new sqaure now"""
+
+    def __init__(self,size):
+        """A new sqaure now"""
+
     self.__size = size
