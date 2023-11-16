b1085e4ba11d514c69b396a7e551973ce4e2080a
lmurayire12 <l.murayire@alustudent.com>
2023-11-16 05:22:45 -0800

    5 square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -11,29 +11,29 @@ class Square:
         ''' init size '''
         self.__size = size
 
-        @property
-        def size(self):
-            "returns the size att"
-            return self.__size
+    @property
+    def size(self):
+        "returns the size att"
+        return self.__size
 
-         @size.setter
-         def size(self, size):
-             '''asign the size to the size att'''
-             if not isinstance(size, int):
-                 raise TypeError("size must be an integer")
-             elif size < 0:
-                 raise ValueError("size must be >= 0")
-             self.__size = size
+    @size.setter
+    def size(self, size):
+        '''asign the size to the size att'''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-        def area(self):
-            square_area = self.__size ** 2
-            return square_area
+    def area(self):
+        square_area = self.__size ** 2
+        return square_area
 
-        def my_print(self):
-            if self.__size == 0:
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
                 print()
-            else:
-                for i in range(self.__size):
-                    for j in range(self.__size):
-                        print("#", end="")
-                    print()
