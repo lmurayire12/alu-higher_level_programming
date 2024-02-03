22f9280c5e57f959f8c5c9b00ec409fb4570e977
lmurayire12 <l.murayire@alustudent.com>
2024-02-03 09:09:32 -0800

    javascript

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-function myArgument(){
-    const numArgument = arguments.length;
-    if (numArgument == 0){
-        console.log('No argument')
-    }
-    else if (numArgument == 1){
-        console.log('Argument found')
-    }
-    else{
-        console.log('Arguments found')
-    }
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
