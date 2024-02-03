d0b77847d944c69dad3398eed018760d55402514
lmurayire12 <l.murayire@alustudent.com>
2024-02-03 08:41:18 -0800

    javascript codes

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-//First line is "C is fun"
 console.log("C is fun");
-//Second line is "Python is cool"
 console.log("Python is cool");
-//Third line is "JavaScript is amazing"
 console.log("JavaScript is amazing");
